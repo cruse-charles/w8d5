c63b3426df93f21f4d11135a3535164ffe5713a2
Tysuiku <timothydong41@gmail.com>
2023-03-03 15:06:37 -0500

    finish myBind

--- argument_exercise.js
+++ argument_exercise.js
@@ -19,8 +19,55 @@ function continuousAdd(num) {
 
 // ----------------------------------- Bind with Args ----------------------
 
-Function.prototype.myBind = function (ctx) {
-  const self = this;
+Function.prototype.myBind = function (ctx, ...bindArgs) {
+  const that = this;
 
-  return function (self) {};
+  return function (...callArgs) {
+    return that.apply(ctx, [...bindArgs, ...callArgs]);
+  };
 };
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
+}
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
