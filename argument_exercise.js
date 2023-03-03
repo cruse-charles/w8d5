3d1cde9c80838318fba1e4031e7bda5bda80c51d
Tysuiku <timothydong41@gmail.com>
2023-03-03 14:58:11 -0500

    start myBind

--- argument_exercise.js
+++ argument_exercise.js
@@ -1,28 +1,26 @@
 function sum(...array) {
-    let total = 0
+  let total = 0;
 
-    for(let i = 0; i < array.length; i++) {
-        total += array[i] 
-    }
-    return total
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 }
-
 
 function continuousAdd(num) {
-    
-    let args = []
+  let args = [];
 
-    return function _curryAdd(num) {
-        args.push(num)
-        console.log(sum(args))
-        // return _curryAdd
-    }
-
-
+  return function _curryAdd(num) {
+    args.push(num);
+    console.log(sum(args));
+    // return _curryAdd
+  };
 }
 
-
-
-
-
 // ----------------------------------- Bind with Args ----------------------
+
+Function.prototype.myBind = function (ctx) {
+  const self = this;
+
+  return function (self) {};
+};
